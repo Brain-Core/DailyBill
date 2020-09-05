2e542ec47b33fb6dd0a0adb2c212b4339585f4d8
glotec <gloireshukurubahati@gmail.com>
2020-09-05 19:55:02 +0300

    Creation_Model_User_Journalier_Service

---
+++ Sys_DailyBill/src/brain/model/ClsUser.java
@@ -0,0 +1,46 @@
+Pakage brain.model;
+
+public Class ClsUser implement IAction
+{
+    private String u_name;
+    private String u_pass;
+    private String u_accreditation;
+
+    public ClsUser(String u_name, String u_pass, String u_accreditation)
+    {
+        this.u_name = u_name;
+        this.u_pass = u_pass;
+        this.u_accreditation = u_accreditation;
+    }
+
+    public String getUName()
+    {
+        return u_name;
+    }
+
+    public void setUName(String u_name)
+    {
+        this.u_name = u_name;
+    }
+
+    public String getUPass()
+    {
+        return u_pass;
+    }
+
+    public void setUPass(String u_pass)
+    {
+        this.u_pass = u_pass;   
+    }
+
+    public String getUAccreditation()
+    {
+        return u_accreditation;
+    }
+
+    private void setUAccreditatin(String u_accreditation) 
+    {
+        this.u_accreditation = u_accreditation;    
+    }
+    
+}
